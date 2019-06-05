b21a2981ddd2fe00283cbbcc0ab355cd6a3a13f9
mcarreon <carryon2095@gmail.com>
2019-06-05 01:13:06 -0700

    Updates

---
+++ precache-manifest.ce989c4fee1e36a9db3b9e27baffc2dd.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ab44610ae62dedb964038546627926a",
+    "url": "/markdown-note-extension/index.html"
+  },
+  {
+    "revision": "bcb683ed36349dc6c132",
+    "url": "/markdown-note-extension/static/css/2.252aea91.chunk.css"
+  },
+  {
+    "revision": "43641d87843f083fabe6",
+    "url": "/markdown-note-extension/static/css/main.603ac91f.chunk.css"
+  },
+  {
+    "revision": "bcb683ed36349dc6c132",
+    "url": "/markdown-note-extension/static/js/2.chunk.js"
+  },
+  {
+    "revision": "c44c72e6ebe5d4c2f010",
+    "url": "/markdown-note-extension/static/js/app.js"
+  },
+  {
+    "revision": "43641d87843f083fabe6",
+    "url": "/markdown-note-extension/static/js/main.chunk.js"
+  },
+  {
+    "revision": "5fa1b8f25b4aa8787f70d5b6b7f0b90f",
+    "url": "/markdown-note-extension/static/media/icons-16.5fa1b8f2.ttf"
+  },
+  {
+    "revision": "6fdb722c94434811f89a68eafc4531f8",
+    "url": "/markdown-note-extension/static/media/icons-16.6fdb722c.eot"
+  },
+  {
+    "revision": "cd1a26696ebf17a89545a3f9067d7028",
+    "url": "/markdown-note-extension/static/media/icons-16.cd1a2669.woff"
+  },
+  {
+    "revision": "14279fa7b78ac2230a015925b603a1f7",
+    "url": "/markdown-note-extension/static/media/icons-20.14279fa7.eot"
+  },
+  {
+    "revision": "657723dc996cf0ae6b4c6110f5d4ddab",
+    "url": "/markdown-note-extension/static/media/icons-20.657723dc.woff"
+  },
+  {
+    "revision": "90247cfdde1bd7b76d804a797d5dec56",
+    "url": "/markdown-note-extension/static/media/icons-20.90247cfd.ttf"
+  }
+]);
